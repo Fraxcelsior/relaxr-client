600f2e42c7d959622bb86fb42fced85c1040aefb
Fraxcelsior <marten_bonnema@hotmail.com>
2019-08-01 14:59:30 +0200

    add iframe to video components

--- src/components/Videos/Video.js
+++ src/components/Videos/Video.js
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom'
 
 export default class Dashboard extends Component {
     render() {
+        if(!this.props.vidId) return 'Loading video...'
         return (
             <div>
                 <h1>Default's videos</h1>
-                <p>How about some comfort in a relaxing video?</p>
-
+                <p>Need some comfort? How about a relaxing video?</p>
+                <iframe width="560" height="315" src={`https://www.youtube.com/embed/${this.props.vidId}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         )
     }
--- src/components/Videos/VideoContainer.js
+++ src/components/Videos/VideoContainer.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Video from './Video'
 
 class VideoContainer extends React.Component {
-
+    state = {
+        vidId: 'IAODG6KaNBc'
+    }
+    
     render() {
         return (
             <div>
-                
+                <Video vidId={this.state.vidId}/>
             </div>
         )
     }
